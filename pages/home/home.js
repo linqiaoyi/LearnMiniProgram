aa0e9b27a2b7c2f023f92fbb5a45ef9e2d5985c9
linqiaoyi <1767854120@qq.com>
2022-08-23 14:18:29 +0800

    09_事件处理

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,10 +5,61 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    titles:['衣服','鞋子','裙子']
   },
   handleBtnClick(){
     console.log('按钮发生点击')
+  },
+  handleTouchStart(){
+    console.log('handleTouchStart')
+  },
+  handleTouchMove(){
+    console.log('handleTouchMove')
+  },
+  handleTouchEnd(){
+    console.log('handleTouchEnd')
+  },
+  handleTap(){
+    console.log('handleTap')
+  },
+  handleLongPress(){
+    console.log('handleLongPress')
+  },
+  handleEventClick(event){
+    console.log('---',event)
+  },
+  handleEventEnd(event){
+    console.log('+++',event)
+  },
+  handleInner(event){
+    console.log('*****',event)
+  },
+  handleOuter(event){
+    console.log('~~~~',event)
+  },
+  handleItemClick(event){
+    const dataset = event.currentTarget.dataset;
+    const title = dataset.item;
+    const index = dataset.index;
+    console.log(title,index)
+  },
+  handleCaptureView1(){
+    console.log('事件捕获1')
+  },
+  handleCaptureView2(){
+    console.log('事件捕获2')
+  },
+  handleCaptureView3(){
+    console.log('事件捕获3')
+  },
+  handleBindView1(){
+    console.log('事件冒泡1')
+  },
+  handleBindView2(){
+    console.log('事件冒泡2')
+  },
+  handleBindView3(){
+    console.log('事件冒泡3')
   },
   /**
    * 生命周期函数--监听页面加载
